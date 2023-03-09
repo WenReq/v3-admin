513933ba5486bdfb563e85d50bc13c4d1266f484
wenreq <294491328.com>
2023-03-09 20:03:06 +0800

    feat(err): 🚀 应用错误处理

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,11 @@ import "element-plus/dist/index.css";
 // 创建vue实例
 const app = createApp(App);
 
+// 应用级错误处理
+app.config.errorHandler = (err, instance, info) => {
+	// 向追踪服务报告错误
+	console.log(err, instance, info);
+};
+
 // 挂载实例
 app.use(router).use(ElementPlus).mount("#app");
