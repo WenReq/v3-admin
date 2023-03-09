1312ea6be0b048b4689275aa1d7e6eb9fb2fe3d2
1332106658@qq.com <1332106658@qq.com>
2023-03-09 11:01:41 +0800

    chore(login.vue): 🔨 remove 123321

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -3,7 +3,7 @@
 	<div>
 		<a href="https://vitejs.dev" target="_blank">
 			<img src="/vite.svg" class="logo" alt="Vite logo" />
-			<!-- test github -->123123
+			<!-- test github -->
 		</a>
 	</div>
 </template>
